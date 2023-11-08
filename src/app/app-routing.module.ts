65b15d0680e8fbfe20a1547f4b5b57a1a84e6961
Romy Gaus <romy.p.gaus@gmail.com>
2023-11-08 18:39:03 +0100

    add route parameters

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { ClipComponent } from './clip/clip.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
+    component: HomeComponent,
   },
   {
     path: 'about',
-    component: AboutComponent
-  }
+    component: AboutComponent,
+  },
+  {
+    path: 'clip/:id',
+    component: ClipComponent,
+  },
 ];
 
 @NgModule({
