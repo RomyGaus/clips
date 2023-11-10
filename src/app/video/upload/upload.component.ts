581e51488baf1fb8e7bf0aee82ae0c67a549398a
Romy Gaus <romy.p.gaus@gmail.com>
2023-11-10 13:01:25 +0100

    add upload title

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-upload',
@@ -10,15 +11,28 @@ export class UploadComponent {
   file: File | null = null;
   nextStep = false;
 
+  title = new FormControl('', {
+    validators: [Validators.required, Validators.minLength(3)],
+    nonNullable: true,
+  });
+  uploadForm = new FormGroup({
+    title: this.title
+  })
+
   storeFile($event: Event) {
     this.isDragover = false;
 
-    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null
+    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null;
 
-    if(!this.file || this.file.type !== 'video/mp4') {
-      return
+    if (!this.file || this.file.type !== 'video/mp4') {
+      return;
     }
 
+    this.title.setValue(this.file.name.replace(/\.[^/.]+$/, ''));
     this.nextStep = true;
+  }
+
+  uploadFile() {
+    console.log('file uploaded')
   }
 }
