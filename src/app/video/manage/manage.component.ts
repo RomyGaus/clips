77283df260f4a0cfda951fcca1b896d672a92818
Romy Gaus <romy.p.gaus@gmail.com>
2023-11-11 14:39:51 +0100

    display clips on manage tab

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Params, Router } from '@angular/router';
-import { ActivatedRoute } from '@angular/router';
+import { Params, Router, ActivatedRoute } from '@angular/router';
+import { ClipService } from 'src/app/service/clip.service';
+import IClip from 'src/app/models/clip.model';
+import { ModalService } from 'src/app/service/modal.service';
 
 @Component({
   selector: 'app-manage',
@@ -9,12 +11,28 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ManageComponent implements OnInit {
   videoOrder = '1';
+  clips: IClip[] = [];
 
-  constructor(private router: Router, private route: ActivatedRoute) {}
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private clipService: ClipService,
+    private modal: ModalService
+  ) {}
 
   ngOnInit(): void {
     this.route.queryParams.subscribe((params: Params) => {
       this.videoOrder = params['sort'] === '2' ? params['sort'] : 1;
+    });
+    this.clipService.getUserClips().subscribe((docs) => {
+      this.clips = [];
+
+      docs.forEach((doc) => {
+        this.clips.push({
+          docID: doc.id,
+          ...doc.data(),
+        });
+      });
     });
   }
 
@@ -27,5 +45,11 @@ export class ManageComponent implements OnInit {
         sort: value,
       },
     });
+  }
+
+  openModal($event: Event, clip: IClip) {
+    $event.preventDefault();
+
+    this.modal.toggleModal('editClip');
   }
 }
