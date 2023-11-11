e4a362d4c0be81ff2ff209d970ca27a200e9065e
Romy Gaus <romy.p.gaus@gmail.com>
2023-11-11 11:11:53 +0100

    create clips service

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { v4 as uuid } from 'uuid';
-import { last } from 'rxjs/operators';
+import { last, switchMap } from 'rxjs/operators';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
+import { ClipService } from 'src/app/service/clip.service';
 
 @Component({
   selector: 'app-upload',
@@ -19,6 +22,7 @@ export class UploadComponent {
   inSubmission = false;
   percentage = 0;
   showPercentage = false;
+  user: firebase.User | null = null;
 
   title = new FormControl('', {
     validators: [Validators.required, Validators.minLength(3)],
@@ -28,7 +32,13 @@ export class UploadComponent {
     title: this.title,
   });
 
-  constructor(private storage: AngularFireStorage) {}
+  constructor(
+    private storage: AngularFireStorage,
+    private auth: AngularFireAuth,
+    private clipsService: ClipService
+  ) {
+    auth.user.subscribe((user) => (this.user = user));
+  }
 
   storeFile($event: Event) {
     this.isDragover = false;
@@ -44,6 +54,7 @@ export class UploadComponent {
   }
 
   uploadFile() {
+    this.uploadForm.disable();
     this.showAlert = true;
     this.alertColor = 'blue';
     this.alertMsg = 'Please wait! Your clip is being uploaded.';
@@ -54,6 +65,7 @@ export class UploadComponent {
     const clipPath = `clips/${clipFileName}.mp4`;
 
     const task = this.storage.upload(clipPath, this.file);
+    const clipRef = this.storage.ref(clipPath);
 
     task.percentageChanges().subscribe((progress) => {
       this.percentage = (progress as number) / 100;
@@ -61,14 +73,30 @@ export class UploadComponent {
 
     task
       .snapshotChanges()
-      .pipe(last())
+      .pipe(
+        last(),
+        switchMap(() => clipRef.getDownloadURL())
+      )
       .subscribe({
-        next: (snapshot) => {
+        next: (url) => {
+          const clip = {
+            uid: this.user?.uid as string,
+            displayName: this.user?.displayName as string,
+            title: this.title.value,
+            fileName: `${clipFileName}.mp4`,
+            url,
+          };
+
+          this.clipsService.createClip(clip);
+
+          console.log(clip);
+
           this.alertColor = 'green';
           this.alertMsg = 'Success! Your clip is ready to share.';
           this.showPercentage = false;
         },
         error: (error) => {
+          this.uploadForm.enable();
           this.alertColor = 'red';
           this.alertMsg = 'Upload failes! Please try again later.';
           this.inSubmission = true;
