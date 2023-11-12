933eb731b00307cd1493928a2677746ec646c08c
Romy Gaus <romy.p.gaus@gmail.com>
2023-11-12 14:46:44 +0100

    binding edit form

--- src/app/video/edit/edit.component.ts
+++ src/app/video/edit/edit.component.ts
@@ -1,12 +1,36 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 import { ModalService } from 'src/app/service/modal.service';
+import IClip from 'src/app/models/clip.model';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { zip } from 'rxjs';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css'],
 })
-export class EditComponent implements OnInit, OnDestroy {
+export class EditComponent implements OnInit, OnDestroy, OnChanges {
+  @Input() activeClip: IClip | null = null;
+
+  clipID = new FormControl('', {
+    nonNullable: true,
+  });
+  title = new FormControl('', {
+    validators: [Validators.required, Validators.minLength(3)],
+    nonNullable: true,
+  });
+  editForm = new FormGroup({
+    title: this.title,
+    id: this.clipID,
+  });
+
   constructor(private modal: ModalService) {}
 
   ngOnInit(): void {
@@ -15,5 +39,14 @@ export class EditComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.modal.unregister('editClip');
+  }
+
+  ngOnChanges(): void {
+    if (!this.activeClip) {
+      return;
+    }
+
+    this.clipID.setValue(this.activeClip.docID as string);
+    this.title.setValue(this.activeClip.title);
   }
 }
